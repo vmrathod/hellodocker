170bb2ec09bd33e70f345c71e18962c1181c2eeb
vmrathod <vishal.amitus@gmail.com>
2015-05-11 17:17:08 +0530

    change in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello from inside a container!');
+    res.send('This is docker test!');
 });
 
 app.listen(8080);
